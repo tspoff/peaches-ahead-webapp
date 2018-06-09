e93eb965bcfc4e95dabaf8fd136f407ef4cff7c3
tspoff <tspoff@gmail.com>
2018-06-09 16:41:00 -0500

    first commit

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,14 @@ import { Router, Route } from 'react-router'
 import App from './components/App';
 import About from './components/About';
 import NotFound from './components/NotFound';
+import Scan from './components/Scan';
+import ChainData from './components/ChainData';
 
 const Routes = (props) => (
   <Router {...props}>
     <Route path="/" component={App} />
+    <Route path="/scan" component={Scan} />
+    <Route path="/data/:multiChainHash" component={ChainData} />
     <Route path="/about" component={About} />
     <Route path="*" component={NotFound} />
   </Router>
